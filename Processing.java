55a432328e9b1c1230acf97e9c9a75b3b6872fff
gregoriustanley <gtejakus@purdue.edu>
2020-10-18 14:16:26 +0700

    processing

---
+++ Processing.java
@@ -0,0 +1,57 @@
+public class Processing {
+    public String course1;
+    public String course2;
+    public String course3;
+    public String course4;
+    public String course5;
+    public String course6;
+    public boolean statement1;
+    public boolean statement2;
+    public boolean statement3;
+    public boolean statement4;
+    public boolean statement5;
+
+    public Processing(){
+    }
+    public String getCourse1() {
+        return course1;
+    }
+    public String getCourse2() {
+        return course2;
+    }
+    public String getCourse3() {
+        return course3;
+    }
+    public String getCourse4() {
+        return course4;
+    }
+    public String getCourse5() {
+        return course5;
+    }
+    public String getCourse6() {
+        return course6;
+    }
+
+    public void setCourse1(String course1) {
+        this.course1 = course1;
+    }
+    public void setCourse2(String course2) {
+        this.course2 = course2;
+    }
+    public void setCourse3(String course3) {
+        this.course3 = course3;
+    }
+    public void setCourse4(String course4) {
+        this.course4 = course4;
+    }
+    public void setCourse5(String course5) {
+        this.course5 = course5;
+    }
+    public void setCourse6(String course6) {
+        this.course6 = course6;
+    }
+
+    public boolean isStatement1() {
+        return statement1;
+    }
+}
